b2414ba1544314776f132eb0e282eb82f4dbf0ce
Jesse Suen <jessesuen@gmail.com>
2019-11-13 18:02:51 -0800

    Fix support for double quotes, newlines, tab arguments in jobs

--- utils/template/template.go
+++ utils/template/template.go
@@ -3,6 +3,7 @@ package query
 import (
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
 
 	"github.com/valyala/fasttemplate"
@@ -40,4 +41,24 @@ func ResolveArgs(template string, args []v1alpha1.Argument) (string, error) {
 		return w.Write([]byte(""))
 	})
 	return s, unresolvedErr
+}
+
+// ResolveQuotedArgs is used for substituting templates which need quotes escaped such as when args
+// are used in JSON which we marshal and unmarshal
+func ResolveQuotedArgs(template string, args []v1alpha1.Argument) (string, error) {
+	quotedArgs := make([]v1alpha1.Argument, len(args))
+	for i, arg := range args {
+		quotedArg := v1alpha1.Argument{
+			Name: arg.Name,
+		}
+		if arg.Value != nil {
+			// The following escapes any special characters (e.g. newlines, tabs, etc...)
+			// in preparation for substitution
+			replacement := strconv.Quote(*arg.Value)
+			replacement = replacement[1 : len(replacement)-1]
+			quotedArg.Value = &replacement
+		}
+		quotedArgs[i] = quotedArg
+	}
+	return ResolveArgs(template, quotedArgs)
 }
