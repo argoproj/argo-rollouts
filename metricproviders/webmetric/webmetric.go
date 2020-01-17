93cd2775973f860e2a37f733f308e9affbf02117
Danny Thomson <dthomson25@gmail.com>
2020-01-17 15:55:32 -0800

    Allow headers and url to be templated

--- metricproviders/webmetric/webmetric.go
+++ metricproviders/webmetric/webmetric.go
@@ -9,12 +9,14 @@ import (
 	"net/url"
 	"time"
 
-	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
-	"github.com/argoproj/argo-rollouts/utils/evaluate"
 	metricutil "github.com/argoproj/argo-rollouts/utils/metric"
 	log "github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/util/jsonpath"
+
+	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
+	"github.com/argoproj/argo-rollouts/utils/evaluate"
+	templateutil "github.com/argoproj/argo-rollouts/utils/template"
 )
 
 const (
@@ -47,16 +49,26 @@ func (p *Provider) Run(run *v1alpha1.AnalysisRun, metric v1alpha1.Metric) v1alph
 	request := &http.Request{
 		Method: "GET", // TODO maybe make this configurable....also implies we will need body templates
 	}
-
-	url, err := url.Parse(metric.Provider.Web.URL)
+	urlStr, err := templateutil.ResolveArgs(metric.Provider.Web.URL, run.Spec.Args)
 	if err != nil {
 		return metricutil.MarkMeasurementError(measurement, err)
 	}
+
+	url, err := url.Parse(urlStr)
+	if err != nil {
+		return metricutil.MarkMeasurementError(measurement, err)
+	}
+
 	request.URL = url
 
 	request.Header = make(http.Header)
+
 	for _, header := range metric.Provider.Web.Headers {
-		request.Header.Set(header.Key, header.Value)
+		value, err := templateutil.ResolveArgs(header.Value, run.Spec.Args)
+		if err != nil {
+			return metricutil.MarkMeasurementError(measurement, err)
+		}
+		request.Header.Set(header.Key, value)
 	}
 
 	// Send Request
