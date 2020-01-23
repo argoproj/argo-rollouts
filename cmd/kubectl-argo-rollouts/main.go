56e6bd9816911b288cfa3eb5af039e16f2b72f47
Dmitrii Evstiukhin <Myaafk@gmail.com>
2020-01-23 12:21:23 -0800

    Update cmd/kubectl-argo-rollouts/main.go
    Co-Authored-By: Bot from GolangCI <42910462+golangcibot@users.noreply.github.com>

--- cmd/kubectl-argo-rollouts/main.go
+++ cmd/kubectl-argo-rollouts/main.go
@@ -6,6 +6,7 @@ import (
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	"k8s.io/klog"
 
