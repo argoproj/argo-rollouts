7e0162e8bee24312bf9fe6cf01061e98bca2f9c1
Samuel Suter <samuel.suter@sendgrid.com>
2020-03-04 01:25:33 -0700

    Cleanup ingress util per review comments

--- utils/ingress/ingress.go
+++ utils/ingress/ingress.go
@@ -8,18 +8,17 @@ import (
 
 // GetRolloutIngressKeys returns ingresses keys (namespace/ingressName) which are referenced by specified rollout
 func GetRolloutIngressKeys(rollout *v1alpha1.Rollout) []string {
-	ingressesSet := make(map[string]bool)
-	if rollout.Spec.Strategy.Canary != nil && rollout.Spec.Strategy.Canary.TrafficRouting != nil && rollout.Spec.Strategy.Canary.TrafficRouting.Nginx != nil {
-		if rollout.Spec.Strategy.Canary.TrafficRouting.Nginx.StableIngress != "" {
-			//log.Errorf("GetRolloutIngressKeys has nginx routing %s: %s", rollout.Name, rollout.Spec.Strategy.Canary.TrafficRouting.Nginx.StableIngress)
-			ingressesSet[fmt.Sprintf("%s/%s", rollout.Namespace, rollout.Spec.Strategy.Canary.TrafficRouting.Nginx.StableIngress)] = true
-			// Also start watcher for `-canary` ingress which is created by the trafficmanagement controller
-			ingressesSet[fmt.Sprintf("%s/%s-canary", rollout.Namespace, rollout.Spec.Strategy.Canary.TrafficRouting.Nginx.StableIngress)] = true
+	if rollout.Spec.Strategy.Canary != nil &&
+		rollout.Spec.Strategy.Canary.TrafficRouting != nil &&
+		rollout.Spec.Strategy.Canary.TrafficRouting.Nginx != nil &&
+		rollout.Spec.Strategy.Canary.TrafficRouting.Nginx.StableIngress != "" {
+
+		// Also start watcher for `-canary` ingress which is created by the trafficmanagement controller
+		return []string{
+			fmt.Sprintf("%s/%s", rollout.Namespace, rollout.Spec.Strategy.Canary.TrafficRouting.Nginx.StableIngress),
+			fmt.Sprintf("%s/%s-canary", rollout.Namespace, rollout.Spec.Strategy.Canary.TrafficRouting.Nginx.StableIngress),
 		}
 	}
-	var ingresses []string
-	for svc := range ingressesSet {
-		ingresses = append(ingresses, svc)
-	}
-	return ingresses
+
+	return nil
 }
