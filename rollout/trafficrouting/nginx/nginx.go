5f5ffe2dbb15adc34282297d837b83b221935783
Samuel Suter <samuel.suter@sendgrid.com>
2020-03-04 17:44:54 -0700

    Fix OwnerRef check and add test

--- rollout/trafficrouting/nginx/nginx.go
+++ rollout/trafficrouting/nginx/nginx.go
@@ -184,11 +184,12 @@ func (r *Reconciler) Reconcile(desiredWeight int32) error {
 
 	// Validate Owner reference matches. If it does not, it means the canary ingress resource
 	// was created outside of this controller and must not be modified
+	r.log.Errorf("owner: %+v", canaryIngress.GetOwnerReferences())
 	if len(canaryIngress.GetOwnerReferences()) == 0 {
 		err := errors.New(fmt.Sprintf("canary ingress %s already exists with no OwnerReferences", canaryIngressName))
 		r.log.WithField(logutil.IngressKey, canaryIngressName).Error(err.Error())
 		return err
-	} else if desiredCanaryIngress.OwnerReferences[0].UID != r.cfg.Rollout.GetUID() {
+	} else if canaryIngress.OwnerReferences[0].UID != r.cfg.Rollout.GetUID() {
 		err := errors.New(fmt.Sprintf("canary ingress %s already exists with different owner", canaryIngressName))
 		r.log.WithField(logutil.IngressKey, canaryIngressName).Error(err.Error())
 		return err
--- rollout/trafficrouting/nginx/nginx_test.go
+++ rollout/trafficrouting/nginx/nginx_test.go
@@ -245,11 +245,31 @@ func TestReconcileStableIngressFound(t *testing.T) {
 	}
 }
 
-func TestReconcileStableAndCanaryIngressFoundBadOwner(t *testing.T) {
+func TestReconcileStableAndCanaryIngressFoundNoOwner(t *testing.T) {
 	rollout := rollout("stable-service", "canary-service", "stable-ingress")
 	stableIngress := ingress("stable-ingress", 80, "stable-service")
 	canaryIngress := ingress("stable-ingress-canary", 80, "canary-service")
 
+	client := fake.NewSimpleClientset(stableIngress, canaryIngress)
+
+	r := NewReconciler(ReconcilerConfig{
+		Rollout:        rollout,
+		Client:         client,
+		Recorder:       &record.FakeRecorder{},
+		ControllerKind: schema.GroupVersionKind{Group: "foo", Version: "v1", Kind: "Bar"},
+	})
+
+	err := r.Reconcile(10)
+	assert.NotNil(t, err, "Reconcile returns error")
+}
+
+func TestReconcileStableAndCanaryIngressFoundBadOwner(t *testing.T) {
+	other_rollout := rollout("stable-service2", "canary-service2", "stable-ingress2")
+	other_rollout.SetUID("4b712b69-5de9-11ea-a10a-0a9ba5899dd2")
+	rollout := rollout("stable-service", "canary-service", "stable-ingress")
+	stableIngress := ingress("stable-ingress", 80, "stable-service")
+	canaryIngress := ingress("stable-ingress-canary", 80, "canary-service")
+	setIngressOwnerRef(canaryIngress, other_rollout)
 	client := fake.NewSimpleClientset(stableIngress, canaryIngress)
 
 	r := NewReconciler(ReconcilerConfig{
