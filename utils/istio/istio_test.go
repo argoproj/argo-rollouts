301d4934faba8dbe9eec9613947b257c0d9d6248
khhirani <kareena.hirani@gmail.com>
2020-08-26 19:40:19 -0700

    Create tests for istioutil

---
+++ utils/istio/istio_test.go
@@ -0,0 +1,23 @@
+package istio
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/runtime"
+	dynamicfake "k8s.io/client-go/dynamic/fake"
+)
+
+func TestDoesIstioExist(t *testing.T) {
+	dynamicClient := dynamicfake.NewSimpleDynamicClient(runtime.NewScheme())
+	assert.True(t, DoesIstioExist(dynamicClient, "v1alpha3"))
+	assert.Len(t, dynamicClient.Actions(), 1)
+	assert.Equal(t, "list", dynamicClient.Actions()[0].GetVerb())
+}
+
+func TestGetIstioGVR(t *testing.T) {
+	gvr := GetIstioGVR("v1alpha3")
+	assert.Equal(t, "networking.istio.io", gvr.Group)
+	assert.Equal(t, "v1alpha3", gvr.Version)
+	assert.Equal(t, "virtualservices", gvr.Resource)
+}
