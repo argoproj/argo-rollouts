0eb4df00df6c50e83d0ea9aa960b9c0970ef2f4f
Eric Mrak <mrak@users.noreply.github.com>
2020-11-09 12:11:52 -0800

    fix: fetch secrets on-demand to fix controller boot for large clusters (#829)
    Using a list cache of secrets is not needed as we do not need to react
    to any changes and we can always fetch the individual secrets we need
    for analysis arguments when the time comes. For large kubernetes
    clusters with thousands of secrets this causes the argo-rollouts
    controller to be unbootable.
    
    Co-authored-by: Jason Yanowitz <jason.yanowitz@getbraintree.com>
    Signed-off-by: Eric Mrak <emrak@paypal.com>

--- cmd/rollouts-controller/main.go
+++ cmd/rollouts-controller/main.go
@@ -129,7 +129,6 @@ func newCommand() *cobra.Command {
 				kubeInformerFactory.Apps().V1().ReplicaSets(),
 				kubeInformerFactory.Core().V1().Services(),
 				kubeInformerFactory.Extensions().V1beta1().Ingresses(),
-				kubeInformerFactory.Core().V1().Secrets(),
 				jobInformerFactory.Batch().V1().Jobs(),
 				tolerantinformer.NewTolerantRolloutInformer(dynamicInformerFactory),
 				tolerantinformer.NewTolerantExperimentInformer(dynamicInformerFactory),
