003b0959ef1486f03f365386b4ef13733122c228
Pedro Arvela <pedro.arvela@onfido.com>
2021-02-23 12:16:32 +0000

    Add watch support to status command
    Signed-off-by: Pedro Arvela <pedro.arvela@onfido.com>

--- pkg/kubectl-argo-rollouts/cmd/status/status.go
+++ pkg/kubectl-argo-rollouts/cmd/status/status.go
@@ -3,6 +3,7 @@ package status
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/argoproj/argo-rollouts/pkg/kubectl-argo-rollouts/info"
 	"github.com/argoproj/argo-rollouts/pkg/kubectl-argo-rollouts/options"
@@ -40,7 +41,8 @@ func NewCmdStatus(o *options.ArgoRolloutsOptions) *cobra.Command {
 			}
 			name := args[0]
 			controller := viewcontroller.NewRolloutViewController(o.Namespace(), name, statusOptions.KubeClientset(), statusOptions.RolloutsClientset())
-			ctx := context.Background()
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
 			controller.Start(ctx)
 
 			ri, err := controller.GetRolloutInfo()
@@ -48,7 +50,17 @@ func NewCmdStatus(o *options.ArgoRolloutsOptions) *cobra.Command {
 				return err
 			}
 
-			statusOptions.PrintStatus(ri)
+			if !statusOptions.Watch {
+				fmt.Println(ri.Status)
+			} else {
+				rolloutUpdates := make(chan *info.RolloutInfo)
+				defer close(rolloutUpdates)
+				controller.RegisterCallback(func(roInfo *info.RolloutInfo) {
+					rolloutUpdates <- roInfo
+				})
+				go statusOptions.WatchStatus(ctx.Done(), cancel, rolloutUpdates)
+				controller.Run(ctx)
+			}
 
 			return nil
 		},
@@ -57,6 +69,27 @@ func NewCmdStatus(o *options.ArgoRolloutsOptions) *cobra.Command {
 	return cmd
 }
 
-func (o *StatusOptions) PrintStatus(roInfo *info.RolloutInfo) {
-	fmt.Println(roInfo.Status)
+func (o *StatusOptions) WatchStatus(stopCh <-chan struct{}, cancelFunc context.CancelFunc, rolloutUpdates chan *info.RolloutInfo) {
+	ticker := time.NewTicker(time.Second)
+	var roInfo *info.RolloutInfo
+	var preventFlicker time.Time
+
+	for {
+		select {
+		case roInfo = <-rolloutUpdates:
+		case <-ticker.C:
+		case <-stopCh:
+			return
+		}
+		if roInfo != nil && time.Now().After(preventFlicker.Add(200*time.Millisecond)) {
+			fmt.Printf("%s - %s\n", roInfo.Status, roInfo.Message)
+
+			if roInfo.Status == "Healthy" || roInfo.Status == "Degraded" {
+				cancelFunc()
+				return
+			}
+
+			preventFlicker = time.Now()
+		}
+	}
 }
