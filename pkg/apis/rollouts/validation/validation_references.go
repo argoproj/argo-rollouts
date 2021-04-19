6c9860da3a475d38e6e0ce7abf4908faa2980a02
khhirani <kareena.hirani@gmail.com>
2021-04-19 15:31:42 -0700

    fix: Validate ClusterAnalysisTemplate args for CAT ref by RO

--- pkg/apis/rollouts/validation/validation_references.go
+++ pkg/apis/rollouts/validation/validation_references.go
@@ -101,11 +101,19 @@ func ValidateAnalysisTemplateWithType(rollout *v1alpha1.Rollout, template Analys
 
 	var templateSpec v1alpha1.AnalysisTemplateSpec
 	var templateName string
+	var args []v1alpha1.Argument
+
 	if template.ClusterAnalysisTemplate != nil {
-		templateName, templateSpec = template.ClusterAnalysisTemplate.Name, template.ClusterAnalysisTemplate.Spec
+		templateName, templateSpec, args = template.ClusterAnalysisTemplate.Name, template.ClusterAnalysisTemplate.Spec, template.ClusterAnalysisTemplate.Spec.Args
 	} else if template.AnalysisTemplate != nil {
-		templateName, templateSpec = template.AnalysisTemplate.Name, template.AnalysisTemplate.Spec
+		templateName, templateSpec, args = template.AnalysisTemplate.Name, template.AnalysisTemplate.Spec, template.AnalysisTemplate.Spec.Args
 	}
+
+	err := analysisutil.ResolveArgs(args)
+	if err != nil {
+		allErrs = append(allErrs, field.Invalid(fldPath, templateName, err.Error()))
+	}
+
 	if template.TemplateType != BackgroundAnalysis {
 		setArgValuePlaceHolder(templateSpec.Args)
 		resolvedMetrics, err := analysisutil.ResolveMetrics(templateSpec.Metrics, templateSpec.Args)
@@ -143,6 +151,7 @@ func ValidateAnalysisTemplateWithType(rollout *v1alpha1.Rollout, template Analys
 			}
 		}
 	}
+
 	return allErrs
 }
 
