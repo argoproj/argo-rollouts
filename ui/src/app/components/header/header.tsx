cfe98953da0d3ed795127b7712bdfe1a17b23010
Remington Breeze <remington@breeze.software>
2021-05-10 11:26:03 -0700

    Outsource reusable UI components to argo-ux npm package
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/app/components/header/header.tsx
+++ ui/src/app/components/header/header.tsx
@@ -1,43 +1,25 @@
-import {faBook, faKeyboard} from '@fortawesome/free-solid-svg-icons';
 import * as React from 'react';
-import {Link, useParams} from 'react-router-dom';
+
+import {ActionButton, Brand, InfoItemRow, ThemeToggle, Tooltip, useData, Header as GenericHeader} from 'argo-ux';
+import {useParams} from 'react-router';
 import {RolloutNamespaceInfo, RolloutServiceApi} from '../../../models/rollout/generated';
 import {RolloutAPIContext} from '../../shared/context/api';
-import {useServerData} from '../../shared/utils/utils';
-import {ActionButton} from '../action-button/action-button';
-import {InfoItemRow} from '../info-item/info-item';
-import {ThemeToggle} from '../theme-toggle/theme-toggle';
-import {Tooltip} from '../tooltip/tooltip';
+import {faBook, faKeyboard} from '@fortawesome/free-solid-svg-icons';
 
 import './header.scss';
+import {Link} from 'react-router-dom';
 
-export const Logo = () => <img src='assets/images/argo-icon-color-square.png' style={{width: '35px', height: '35px', margin: '0 8px'}} alt='Argo Logo' />;
-
-const Brand = (props: {path?: string}) => {
-    const [loading, setLoading] = React.useState(true);
-    React.useEffect(() => {
-        setTimeout(() => setLoading(false), 500);
-    }, []);
-
-    const showWelcome = loading && !props.path;
-    return (
-        <Link to='/' className='rollouts-header__brand'>
-            <Logo />
-            <h1>
-                <div className='rollouts-header__welcome' style={{opacity: showWelcome ? 1 : 0, transform: showWelcome ? 'none' : 'scaleX(0.01)'}}>
-                    Welcome to Argo
-                </div>
-                <div className='rollouts-header__title' style={showWelcome ? {} : {transform: 'translateX(0)'}}>
-                    Rollouts {props.path && <h2> / {props.path} </h2>}
-                </div>
-            </h1>
-        </Link>
-    );
-};
+const Logo = () => <img src='assets/images/argo-icon-color-square.png' style={{width: '35px', height: '35px', margin: '0 8px'}} alt='Argo Logo' />;
 
 export const Header = (props: {pageHasShortcuts: boolean; showHelp: () => void}) => {
     const getNs = React.useCallback(() => new RolloutServiceApi().rolloutServiceGetNamespace(), []);
-    const namespace = useServerData<RolloutNamespaceInfo>(getNs).namespace || 'Unknown';
+    const [nsData, loading] = useData<RolloutNamespaceInfo>(getNs);
+    const [namespace, setNamespace] = React.useState('Unknown');
+    React.useEffect(() => {
+        if (!loading && nsData && nsData.namespace) {
+            setNamespace(nsData.namespace);
+        }
+    }, [nsData, loading]);
     const {name} = useParams<{name: string}>();
     const api = React.useContext(RolloutAPIContext);
     const [version, setVersion] = React.useState('v?');
@@ -49,27 +31,29 @@ export const Header = (props: {pageHasShortcuts: boolean; showHelp: () => void})
         getVersion();
     });
     return (
-        <header className='rollouts-header'>
-            <Brand path={name} />
+        <GenericHeader>
+            <Link to='/'>
+                <Brand path={name} brandName='Argo Rollouts' logo={<Logo />} />
+            </Link>
             <div className='rollouts-header__info'>
                 {props.pageHasShortcuts && (
-                    <Tooltip content='Keyboard Shortcuts' inverted>
-                        <ActionButton icon={faKeyboard} action={props.showHelp} dark />
+                    <Tooltip content='Keyboard Shortcuts' inverted={true}>
+                        <ActionButton icon={faKeyboard} action={props.showHelp} dark={true} />
                     </Tooltip>
                 )}
-                <Tooltip content='Documentation' inverted>
+                <Tooltip content='Documentation' inverted={true}>
                     <a href='https://argoproj.github.io/argo-rollouts/' target='_blank' rel='noreferrer'>
-                        <ActionButton icon={faBook} dark />
+                        <ActionButton icon={faBook} dark={true} />
                     </a>
                 </Tooltip>
                 <span style={{marginRight: '7px'}}>
-                    <Tooltip content='Toggle Dark Mode' inverted>
+                    <Tooltip content='Toggle Dark Mode' inverted={true}>
                         <ThemeToggle />
                     </Tooltip>
                 </span>
                 <InfoItemRow label={'NS:'} items={{content: namespace}} />
                 <div className='rollouts-header__version'>{version}</div>
             </div>
-        </header>
+        </GenericHeader>
     );
 };
