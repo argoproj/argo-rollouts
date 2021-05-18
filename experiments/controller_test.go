1f1614037718277bd1fdb3a6a08404255eed8744
khhirani <kareena.hirani@gmail.com>
2021-05-18 01:53:45 -0700

    Modify experiments test
    Signed-off-by: khhirani <kareena.hirani@gmail.com>

--- experiments/controller_test.go
+++ experiments/controller_test.go
@@ -332,6 +332,7 @@ func (f *fixture) newController(resync resyncFunc) (*Controller, informers.Share
 		AnalysisRunInformer:             i.Argoproj().V1alpha1().AnalysisRuns(),
 		AnalysisTemplateInformer:        i.Argoproj().V1alpha1().AnalysisTemplates(),
 		ClusterAnalysisTemplateInformer: i.Argoproj().V1alpha1().ClusterAnalysisTemplates(),
+		ServiceInformer:                 k8sI.Core().V1().Services(),
 		ResyncPeriod:                    resync(),
 		RolloutWorkQueue:                rolloutWorkqueue,
 		ExperimentWorkQueue:             experimentWorkqueue,
