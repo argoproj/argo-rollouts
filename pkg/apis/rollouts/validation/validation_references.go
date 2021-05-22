57c7cffde695bb5dc9fe7c524de7812e5a92639b
khhirani <kareena.hirani@gmail.com>
2021-05-21 19:39:17 -0700

    Only resolve Analysis metrics in validation_references.go
    Signed-off-by: khhirani <kareena.hirani@gmail.com>

--- pkg/apis/rollouts/validation/validation_references.go
+++ pkg/apis/rollouts/validation/validation_references.go
@@ -2,7 +2,6 @@ package validation
 
 import (
 	"fmt"
-
 	analysisutil "github.com/argoproj/argo-rollouts/utils/analysis"
 	"github.com/argoproj/argo-rollouts/utils/conditions"
 	serviceutil "github.com/argoproj/argo-rollouts/utils/service"
@@ -138,9 +137,8 @@ func ValidateAnalysisTemplateWithType(rollout *v1alpha1.Rollout, template *v1alp
 	}
 
 	if templateType != BackgroundAnalysis {
-		templateSpec = *templateSpec.DeepCopy()
 		setArgValuePlaceHolder(templateSpec.Args)
-		resolvedMetrics, err := analysisutil.ResolveMetrics(templateSpec.Metrics, templateSpec.Args)
+		resolvedMetrics, err := validateAnalysisMetrics(templateSpec.Metrics, templateSpec.Args)
 		if err != nil {
 			msg := fmt.Sprintf("AnalysisTemplate %s: %v", templateName, err)
 			allErrs = append(allErrs, field.Invalid(fldPath, templateName, msg))
@@ -294,3 +292,26 @@ func GetAnalysisTemplateWithTypeFieldPath(templateType AnalysisTemplateType, can
 	}
 	return fldPath
 }
+
+// validateAnalysisMetrics validates the metrics of an Analysis object
+func validateAnalysisMetrics(metrics []v1alpha1.Metric, args []v1alpha1.Argument) ([]v1alpha1.Metric, error) {
+	for i, arg := range args {
+		if arg.ValueFrom != nil {
+			if arg.Value != nil {
+				return nil, fmt.Errorf("arg '%s' has both Value and ValueFrom fields", arg.Name)
+			}
+			argVal := "dummy-value"
+			args[i].Value = &argVal
+		}
+	}
+
+	for i, metric := range metrics {
+		resolvedMetric, err := analysisutil.ResolveMetricArgs(metric, args)
+		if err != nil {
+			return nil, err
+		}
+		metrics[i] = *resolvedMetric
+	}
+	return metrics, nil
+}
+
