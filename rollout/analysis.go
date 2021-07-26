01536ea98009634205bdd9cf8e684832186cfbcc
Andrii Perenesenko <andrii.perenesenko@gmail.com>
2021-07-26 11:47:00 -0700

    lint fix sorting import
    Signed-off-by: Andrii Perenesenko <andrii.perenesenko@gmail.com>

--- rollout/analysis.go
+++ rollout/analysis.go
@@ -3,8 +3,6 @@ package rollout
 import (
 	"context"
 	"fmt"
-
-	"github.com/argoproj/argo-rollouts/utils/defaults"
 	"strconv"
 	"strings"
 
@@ -19,6 +17,7 @@ import (
 	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
 	analysisutil "github.com/argoproj/argo-rollouts/utils/analysis"
 	"github.com/argoproj/argo-rollouts/utils/annotations"
+	"github.com/argoproj/argo-rollouts/utils/defaults"
 	logutil "github.com/argoproj/argo-rollouts/utils/log"
 	"github.com/argoproj/argo-rollouts/utils/record"
 	replicasetutil "github.com/argoproj/argo-rollouts/utils/replicaset"
