f2d75b0be2b6a4d1a0ae937fd4436b60015c8d49
Mike Ball <mikedball@gmail.com>
2021-08-02 08:40:03 -0400

    add graphite.APIClient#Query test
    Signed-off-by: Mike Ball <mikedball@gmail.com>

--- metricproviders/graphite/api_test.go
+++ metricproviders/graphite/api_test.go
@@ -2,6 +2,8 @@ package graphite
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
@@ -33,4 +35,69 @@ func TestNewAPIWithInvalidURL(t *testing.T) {
 
 	assert.Equal(t, err.Error(), fmt.Sprintf("Graphite address %s is not a valid URL", addr))
 	assert.Nil(t, g)
+}
+
+func TestQuery(t *testing.T) {
+	tests := []struct {
+		name           string
+		query          string
+		expectedTarget string
+		expectedFrom   string
+		expectedResult float64
+		body           string
+	}{{
+		"ok",
+		"target=sumSeries(app.http.*.*.count)&from=-2min",
+		"sumSeries(app.http.*.*.count)",
+		"-2min",
+		float64(100),
+		`[
+			{
+				"datapoints": [
+					[
+						10,
+						1621348400
+					],
+					[
+						75,
+						1621348410
+					],
+					[
+						25,
+						1621348420
+					],
+					[
+						100,
+						1621348430
+					]
+				],
+				"target": "sumSeries(app.http.*.*.count)",
+				"tags": {
+					"aggregatedBy": "sum",
+					"name": "sumSeries(app.http.*.*.count)"
+				}
+			}
+		]`,
+	}}
+
+	for _, test := range tests {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			target := r.URL.Query().Get("target")
+			assert.Equal(t, test.expectedTarget, target)
+
+			from := r.URL.Query().Get("from")
+			assert.Equal(t, test.expectedFrom, from)
+
+			json := test.body
+			w.Write([]byte(json))
+		}))
+		defer ts.Close()
+
+		g, err := NewAPIClient(testGraphiteMetric(ts.URL), log.Entry{})
+		assert.Nil(t, err)
+
+		val, err := g.Query(test.query)
+		assert.Nil(t, err)
+		assert.Equal(t, &test.expectedResult, val)
+	}
 }
