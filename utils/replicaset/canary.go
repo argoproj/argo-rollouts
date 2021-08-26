ee060b39c87343c9f8a9b64577b762d5a5fc00f3
hari rongali <hari_rongali@intuit.com>
2021-08-25 17:52:53 -0700

    fix: lint
    Signed-off-by: hari rongali <hari_rongali@intuit.com>

--- utils/replicaset/canary.go
+++ utils/replicaset/canary.go
@@ -191,7 +191,7 @@ func CalculateReplicaCountsForCanary(rollout *v1alpha1.Rollout, newRS *appsv1.Re
 		// Skip scalingDown Stable replicaSet when Canary availability is not taken into calculation for scaleDown
 		newRSReplicaCount = calculateScaleDownReplicaCount(newRS, desiredNewRSReplicaCount, scaleDownCount, newRSReplicaCount)
 		newRSReplicaCount, stableRSReplicaCount = adjustReplicaWithinLimits(newRS, stableRS, newRSReplicaCount, stableRSReplicaCount, maxReplicaCountAllowed, minAvailableReplicaCount)
-	} else if scaleStableRS{
+	} else if scaleStableRS {
 		// Skip scalingDown canary replicaSet when StableSet availability is not taken into calculation for scaleDown
 		stableRSReplicaCount = calculateScaleDownReplicaCount(stableRS, desiredStableRSReplicaCount, scaleDownCount, stableRSReplicaCount)
 		stableRSReplicaCount, newRSReplicaCount = adjustReplicaWithinLimits(stableRS, newRS, stableRSReplicaCount, newRSReplicaCount, maxReplicaCountAllowed, minAvailableReplicaCount)
