3390e417b1772957028e7e798d114f71eee02024
fblgit <info@fblnet.net>
2021-09-24 23:46:08 +0000

    fix: missing Ptr on singleTrafficReconciler
    Signed-off-by: fblgit <info@fblnet.net>

--- rollout/trafficrouting_test.go
+++ rollout/trafficrouting_test.go
@@ -43,7 +43,7 @@ func newFakeSingleTrafficRoutingReconciler() *mocks.TrafficRoutingReconciler {
 	trafficRoutingReconciler := mocks.TrafficRoutingReconciler{}
 	trafficRoutingReconciler.On("Type").Return("fake")
 	trafficRoutingReconciler.On("SetWeight", mock.Anything).Return(nil)
-	trafficRoutingReconciler.On("VerifyWeight", mock.Anything).Return(true, nil)
+	trafficRoutingReconciler.On("VerifyWeight", mock.Anything).Return(pointer.BoolPtr(true), nil)
 	trafficRoutingReconciler.On("UpdateHash", mock.Anything, mock.Anything).Return(nil)
 	return &trafficRoutingReconciler
 }
