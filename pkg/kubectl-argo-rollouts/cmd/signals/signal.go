fd171c29b081f2e2a44bbb80000b988bc0b21633
Hui Kang <hui.kang@salesforce.com>
2021-10-14 15:43:30 -0400

    fix unit test
    Signed-off-by: Hui Kang <hui.kang@salesforce.com>

--- pkg/kubectl-argo-rollouts/cmd/signals/signal.go
+++ pkg/kubectl-argo-rollouts/cmd/signals/signal.go
@@ -7,11 +7,7 @@ import (
 	"syscall"
 )
 
-var onlyOneSignalHandler = make(chan struct{})
-
 func SetupSignalHandler(cancel context.CancelFunc) {
-	close(onlyOneSignalHandler) // panics when called twice
-
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
