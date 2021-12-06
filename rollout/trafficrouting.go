166230fb7f09a96664bf430727b8db44ca6bf981
Nissan Hefetz <nissy34@gmail.com>
2021-12-06 10:30:39 +0200

    On promote-full calculate the weight by (100 * canaryAvailablereplicas) / Replica. This will make sure we will not have an outage when we do promote full.
    Signed-off-by: Nissan Hefetz <nissy34@gmail.com>

--- rollout/trafficrouting.go
+++ rollout/trafficrouting.go
@@ -126,6 +126,10 @@ func (c *rolloutContext) reconcileTrafficRouting() error {
 			if c.rollout.Spec.Strategy.Canary.DynamicStableScale {
 				desiredWeight = 100 - ((100 * c.stableRS.Status.AvailableReplicas) / *c.rollout.Spec.Replicas)
 			}
+		} else if c.rollout.Status.PromoteFull {
+			// on a promote full, desired stable weight should be 0 (100% to canary),
+			// But we can only increase canary weight according to available replica counts of the canary.
+			desiredWeight = (100 * c.newRS.Status.AvailableReplicas) / *c.rollout.Spec.Replicas
 		} else if c.newRS == nil || c.newRS.Status.AvailableReplicas == 0 {
 			// when newRS is not available or replicas num is 0. never weight to canary
 			weightDestinations = append(weightDestinations, c.calculateWeightDestinationsFromExperiment()...)
