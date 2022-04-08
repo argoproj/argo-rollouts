e51dc00766c3c18fe7d61ffd6cb62421740ecea7
zachaller <zachaller@hotmail.com>
2022-04-08 14:50:54 -0500

    pre commit check
    Signed-off-by: zachaller <zachaller@hotmail.com>

--- utils/aws/aws.go
+++ utils/aws/aws.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
 
 	"github.com/argoproj/argo-rollouts/utils/defaults"
 	"github.com/aws/aws-sdk-go-v2/config"
