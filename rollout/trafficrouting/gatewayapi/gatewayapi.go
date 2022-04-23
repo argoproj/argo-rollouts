50ce872375ea8351604a00625083489efedae049
PhilippPlotnikov <70917163+PhilippDeveloper@users.noreply.github.com>
2022-04-24 00:14:01 +0500

    Add gateway api
    Signed-off-by: PhilippPlotnikov <70917163+PhilippDeveloper@users.noreply.github.com>

--- rollout/trafficrouting/gatewayapi/gatewayapi.go
+++ rollout/trafficrouting/gatewayapi/gatewayapi.go
@@ -1,1 +1,23 @@
 package gatewayapi
+
+// Type holds this controller type
+const Type = "GatewayAPI"
+
+type ReconcilerConfig struct {
+
+}
+
+type Reconciler struct {}
+
+func NewReconciler(cfg *ReconcilerConfig) *Reconciler {
+	reconciler := Reconciler{}
+	return &reconciler
+}
+
+func (r *Reconciler) SetWeight() {}
+
+func (r *Reconciler) VerifyWeight() {}
+
+func (r *Reconciler) Type() string {
+	return Type
+}
