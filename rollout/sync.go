af661cd4f1ba5660ac9d5098766c41677e534cfc
Ravi Hari <ravireliable@gmail.com>
2022-04-26 19:32:41 +0530

    fix: UpdateRollout runtime object with typemeta fields
    Signed-off-by: Ravi Hari <ravireliable@gmail.com>

--- rollout/sync.go
+++ rollout/sync.go
@@ -3,6 +3,7 @@ package rollout
 import (
 	"context"
 	"fmt"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sort"
 	"strconv"
 	"time"
@@ -102,6 +103,14 @@ func (c *rolloutContext) syncReplicaSetRevision() (*appsv1.ReplicaSet, error) {
 		condition := conditions.NewRolloutCondition(v1alpha1.RolloutProgressing, corev1.ConditionTrue, conditions.FoundNewRSReason, msg)
 		conditions.SetRolloutCondition(&c.rollout.Status, *condition)
 		updatedRollout, err := c.argoprojclientset.ArgoprojV1alpha1().Rollouts(c.rollout.Namespace).UpdateStatus(ctx, c.rollout, metav1.UpdateOptions{})
+		// adding this block as argoprojclientset update deletes typemeta fields.
+		if updatedRollout != nil {
+			updatedRollout.GetObjectKind().SetGroupVersionKind(schema.GroupVersionKind{
+				Group:   "argoproj.io",
+				Kind:    "Rollout",
+				Version: "v1alpha1",
+			})
+		}
 		if err != nil {
 			c.log.WithError(err).Error("Error: updating rollout revision")
 			return nil, err
@@ -119,6 +128,14 @@ func (c *rolloutContext) setRolloutRevision(revision string) error {
 		if err != nil {
 			c.log.WithError(err).Error("Error: updating rollout revision")
 			return err
+		}
+		// adding this block as argoprojclientset update deletes typemeta fields.
+		if updatedRollout != nil {
+			updatedRollout.GetObjectKind().SetGroupVersionKind(schema.GroupVersionKind{
+				Group:   "argoproj.io",
+				Kind:    "Rollout",
+				Version: "v1alpha1",
+			})
 		}
 		c.rollout = updatedRollout.DeepCopy()
 		if err := c.refResolver.Resolve(c.rollout); err != nil {
@@ -256,6 +273,14 @@ func (c *rolloutContext) createDesiredReplicaSet() (*appsv1.ReplicaSet, error) {
 		updatedRollout, err := c.argoprojclientset.ArgoprojV1alpha1().Rollouts(c.rollout.Namespace).UpdateStatus(ctx, c.rollout, metav1.UpdateOptions{})
 		if err != nil {
 			return nil, err
+		}
+		// adding this block as argoprojclientset update deletes typemeta fields.
+		if updatedRollout != nil {
+			updatedRollout.GetObjectKind().SetGroupVersionKind(schema.GroupVersionKind{
+				Group:   "argoproj.io",
+				Kind:    "Rollout",
+				Version: "v1alpha1",
+			})
 		}
 		c.rollout = updatedRollout.DeepCopy()
 		if err := c.refResolver.Resolve(c.rollout); err != nil {
