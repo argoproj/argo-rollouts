4e06201009085aa3bd6bb47339a56af27e7c3e13
PhilippPlotnikov <philipp.plotnikov@codefresh.io>
2022-04-27 15:18:32 +0500

    Add tests
    Signed-off-by: PhilippPlotnikov <philipp.plotnikov@codefresh.io>

--- rollout/trafficrouting/gatewayapi/gatewayapi_test.go
+++ rollout/trafficrouting/gatewayapi/gatewayapi_test.go
@@ -3,32 +3,74 @@ package gatewayapi
 import (
 	"testing"
 
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	stableServiceName string = "stable-rollout"
+	canaryServiceName string = "canary-rollout"
+	httpRouteName     string = "http-route"
 )
 
 func TestSetWeight(t *testing.T) {}
 
 func TestVerifyWeight(t *testing.T) {
+	t.Run("VerifyWeight", func(t *testing.T) {
+		// Given
+		t.Parallel()
+		cfg := ReconcilerConfig{
+			Rollout: newRollout(stableServiceName, canaryServiceName, httpRouteName),
+		}
+		r := NewReconciler(&cfg)
 
+		// When
+		isSynced, err := r.VerifyWeight(32)
+
+		// Then
+		assert.Nil(t, isSynced)
+		assert.Nil(t, err)
+	})
 }
 
 func TestType(t *testing.T) {
+	t.Run("Type", func(t *testing.T) {
+		// Given
+		t.Parallel()
+		cfg := ReconcilerConfig{
+			Rollout: newRollout(stableServiceName, canaryServiceName, httpRouteName),
+		}
+		r := NewReconciler(&cfg)
 
+		// When
+		reconcilerType := r.Type()
+
+		// Then
+		assert.Equal(t, Type, reconcilerType)
+	})
 }
 
-func TestGetService(t *testing.T) {
+func TestGetService(t *testing.T) {}
 
-}
-
-func toUnstructured(t *testing.T, manifest string) *unstructured.Unstructured {
-	t.Helper()
-	obj := &unstructured.Unstructured{}
-
-	dec := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
-	_, _, err := dec.Decode([]byte(manifest), nil, obj)
-	if err != nil {
-		t.Fatal(err)
+func newRollout(stableSvc, canarySvc, httpRouteName string) *v1alpha1.Rollout {
+	return &v1alpha1.Rollout{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "rollout",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.RolloutSpec{
+			Strategy: v1alpha1.RolloutStrategy{
+				Canary: &v1alpha1.CanaryStrategy{
+					StableService: stableSvc,
+					CanaryService: canarySvc,
+					TrafficRouting: &v1alpha1.RolloutTrafficRouting{
+						GatewayAPI: &v1alpha1.GatewayAPITrafficRouting{
+							HTTPRoute: httpRouteName,
+						},
+					},
+				},
+			},
+		},
 	}
-	return obj
 }
