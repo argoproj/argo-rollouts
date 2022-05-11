538a24fc034a5e125292c502adc05f9238cc435c
zachaller <zachaller@hotmail.com>
2022-05-11 15:40:51 -0500

    WIP: Have partial working multiple rollout resources in the same manifest
    Signed-off-by: zachaller <zachaller@hotmail.com>

--- pkg/kubectl-argo-rollouts/cmd/lint/lint.go
+++ pkg/kubectl-argo-rollouts/cmd/lint/lint.go
@@ -2,33 +2,36 @@ package lint
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
-	ingressutil "github.com/argoproj/argo-rollouts/utils/ingress"
-	"io"
-	"io/ioutil"
-	v1 "k8s.io/api/core/v1"
-	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
-	networkingv1 "k8s.io/api/networking/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/validation/field"
-	"unicode"
-
 	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts"
 	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
 	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts/validation"
 	"github.com/argoproj/argo-rollouts/pkg/kubectl-argo-rollouts/options"
+	ingressutil "github.com/argoproj/argo-rollouts/utils/ingress"
 	"github.com/ghodss/yaml"
 	"github.com/spf13/cobra"
 	goyaml "gopkg.in/yaml.v2"
+	"io"
+	"io/ioutil"
 	istioNetworkingv1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
 	istioNetworkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
+	v1 "k8s.io/api/core/v1"
+	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
+	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
 type LintOptions struct {
 	options.ArgoRolloutsOptions
 	File string
+}
+
+type roAndReferences struct {
+	Rollout    v1alpha1.Rollout
+	References validation.ReferencedResources
 }
 
 const (
@@ -65,22 +68,7 @@ func NewCmdLint(o *options.ArgoRolloutsOptions) *cobra.Command {
 	return cmd
 }
 
-// isJSON detects if the byte array looks like json, based on the first non-whitespace character
-func isJSON(fileBytes []byte) bool {
-	for _, b := range fileBytes {
-		if !unicode.IsSpace(rune(b)) {
-			return b == '{'
-		}
-	}
-	return false
-}
-
 func unmarshal(fileBytes []byte, obj interface{}) error {
-	if isJSON(fileBytes) {
-		decoder := json.NewDecoder(bytes.NewReader(fileBytes))
-		decoder.DisallowUnknownFields()
-		return decoder.Decode(&obj)
-	}
 	return yaml.UnmarshalStrict(fileBytes, &obj, yaml.DisallowUnknownFields)
 }
 
@@ -91,11 +79,8 @@ func (l *LintOptions) lintResource(path string) error {
 	}
 
 	var un unstructured.Unstructured
+	var refResource validation.ReferencedResources
 	var fileRollouts []v1alpha1.Rollout
-	var fileServices []validation.ServiceWithType
-	var fileIngresses []ingressutil.Ingress
-	var fileAnalysisTemplates []validation.AnalysisTemplatesWithType
-	var fileVirtualServices []unstructured.Unstructured
 
 	decoder := goyaml.NewDecoder(bytes.NewReader(fileBytes))
 	for {
@@ -119,133 +104,199 @@ func (l *LintOptions) lintResource(path string) error {
 		}
 
 		gvk := un.GroupVersionKind()
-		switch {
-		case gvk.Group == rollouts.Group && gvk.Kind == rollouts.RolloutKind:
+		if gvk.Group == rollouts.Group && gvk.Kind == rollouts.RolloutKind {
 			var ro v1alpha1.Rollout
 			err := unmarshal(valueBytes, &ro)
 			if err != nil {
 				return err
 			}
 			fileRollouts = append(fileRollouts, ro)
-
-		case gvk.Group == v1.GroupName && gvk.Kind == "Service":
-			var svc v1.Service
-			err := unmarshal(valueBytes, &svc)
-			if err != nil {
-				return err
-			}
-			fileServices = append(fileServices, validation.ServiceWithType{
-				Service: &svc,
-			})
-
-		case gvk.Group == istioNetworkingv1beta1.GroupName && gvk.Kind == "VirtualService":
-			var virtualServicev1beta1 istioNetworkingv1beta1.VirtualService
-			var virtualServicev1alpha3 istioNetworkingv1alpha3.VirtualService
-			if gvk.Version == "v1alpha3" {
-				err := unmarshal(valueBytes, &virtualServicev1alpha3)
-				if err != nil {
-					return err
-				}
-
-				vsvcUn, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&virtualServicev1alpha3)
-				if err != nil {
-					return err
-				}
-				fileVirtualServices = append(fileVirtualServices, unstructured.Unstructured{
-					Object: vsvcUn,
-				})
-			} else if gvk.Version == "v1beta1" {
-				err := unmarshal(valueBytes, &virtualServicev1beta1)
-				if err != nil {
-					return err
-				}
-				vsvcUn, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&virtualServicev1beta1)
-				if err != nil {
-					return err
-				}
-				fileVirtualServices = append(fileVirtualServices, unstructured.Unstructured{
-					Object: vsvcUn,
-				})
-			}
-
-		case (gvk.Group == networkingv1.GroupName || gvk.Group == extensionsv1beta1.GroupName) && gvk.Kind == "Ingress":
-			var ing networkingv1.Ingress
-			var ingv1beta1 extensionsv1beta1.Ingress
-			if gvk.Version == "v1" {
-				err := unmarshal(valueBytes, &ing)
-				if err != nil {
-					return err
-				}
-				fileIngresses = append(fileIngresses, *ingressutil.NewIngress(&ing))
-			} else if gvk.Version == "v1beta1" {
-				err := unmarshal(valueBytes, &ingv1beta1)
-				if err != nil {
-					return err
-				}
-				fileIngresses = append(fileIngresses, *ingressutil.NewLegacyIngress(&ingv1beta1))
-			}
-
 		}
-	}
-	
-	refResource := validation.ReferencedResources{
-		AnalysisTemplatesWithType: fileAnalysisTemplates,
-		Ingresses:                 fileIngresses,
-		ServiceWithType:           fileServices,
-		VirtualServices:           fileVirtualServices,
-		AmbassadorMappings:        nil,
-		AppMeshResources:          nil,
+		err = buildAllReferencedResources(gvk, valueBytes, &refResource)
+		if err != nil {
+			return err
+		}
 	}
 
 	setServiceTypeAndManagedAnnotation(fileRollouts, refResource)
+	setIngressManagedAnnotation(fileRollouts, refResource)
 
 	var errList field.ErrorList
 	for _, rollout := range fileRollouts {
-		errList = append(errList, validation.ValidateRollout(&rollout)...)
-		errList = append(errList, validation.ValidateRolloutReferencedResources(&rollout, refResource)...)
+		roRef := matchRolloutToReferences(rollout, refResource)
+		errList = append(errList, validation.ValidateRollout(&roRef.Rollout)...)
+		errList = append(errList, validation.ValidateRolloutReferencedResources(&roRef.Rollout, roRef.References)...)
 	}
 
 	for _, e := range errList {
 		fmt.Println(e.ErrorBody())
 	}
-	if len(errList) > 1 {
+	if len(errList) > 0 {
 		return errList[0]
 	} else {
 		return nil
 	}
 }
 
+// buildAllReferencedResources This builds a ReferencedResources object that has all the external resources for every
+// rollout resource in the manifest. We will need to later match each referenced resource to its own rollout resource
+// before passing the rollout object and its managed reference on to validation.
+func buildAllReferencedResources(gvk schema.GroupVersionKind, valueBytes []byte, refResource *validation.ReferencedResources) error {
+	switch {
+	case gvk.Group == v1.GroupName && gvk.Kind == "Service":
+		var svc v1.Service
+		err := unmarshal(valueBytes, &svc)
+		if err != nil {
+			return err
+		}
+		refResource.ServiceWithType = append(refResource.ServiceWithType, validation.ServiceWithType{
+			Service: &svc,
+		})
+
+	case gvk.Group == istioNetworkingv1beta1.GroupName && gvk.Kind == "VirtualService":
+		var virtualServicev1beta1 istioNetworkingv1beta1.VirtualService
+		var virtualServicev1alpha3 istioNetworkingv1alpha3.VirtualService
+		if gvk.Version == "v1alpha3" {
+			err := unmarshal(valueBytes, &virtualServicev1alpha3)
+			if err != nil {
+				return err
+			}
+
+			vsvcUn, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&virtualServicev1alpha3)
+			if err != nil {
+				return err
+			}
+			refResource.VirtualServices = append(refResource.VirtualServices, unstructured.Unstructured{
+				Object: vsvcUn,
+			})
+		} else if gvk.Version == "v1beta1" {
+			err := unmarshal(valueBytes, &virtualServicev1beta1)
+			if err != nil {
+				return err
+			}
+			vsvcUn, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&virtualServicev1beta1)
+			if err != nil {
+				return err
+			}
+			refResource.VirtualServices = append(refResource.VirtualServices, unstructured.Unstructured{
+				Object: vsvcUn,
+			})
+		}
+
+	case (gvk.Group == networkingv1.GroupName || gvk.Group == extensionsv1beta1.GroupName) && gvk.Kind == "Ingress":
+		var ing networkingv1.Ingress
+		var ingv1beta1 extensionsv1beta1.Ingress
+		if gvk.Version == "v1" {
+			err := unmarshal(valueBytes, &ing)
+			if err != nil {
+				return err
+			}
+			refResource.Ingresses = append(refResource.Ingresses, *ingressutil.NewIngress(&ing))
+		} else if gvk.Version == "v1beta1" {
+			err := unmarshal(valueBytes, &ingv1beta1)
+			if err != nil {
+				return err
+			}
+			refResource.Ingresses = append(refResource.Ingresses, *ingressutil.NewLegacyIngress(&ingv1beta1))
+		}
+
+	}
+	return nil
+}
+
+// matchRolloutToReferences This function goes through the global list of all ReferencedResources in the manifest and matches
+// them up with their respective rollout object so that we can latter have a mapping of a single rollout object and its
+// referenced resources.
+func matchRolloutToReferences(rollout v1alpha1.Rollout, refResource validation.ReferencedResources) roAndReferences {
+	matchedReferenceResources := roAndReferences{Rollout: rollout, References: validation.ReferencedResources{}}
+
+	for _, service := range refResource.ServiceWithType {
+		if service.Service.Annotations[v1alpha1.ManagedByRolloutsKey] == rollout.Name {
+			matchedReferenceResources.References.ServiceWithType = append(matchedReferenceResources.References.ServiceWithType, service)
+		}
+	}
+	for _, ingress := range refResource.Ingresses {
+		if ingress.GetAnnotations()[v1alpha1.ManagedByRolloutsKey] == rollout.Name {
+			matchedReferenceResources.References.Ingresses = append(matchedReferenceResources.References.Ingresses, ingress)
+		}
+	}
+
+	matchedReferenceResources.References.AnalysisTemplatesWithType = refResource.AnalysisTemplatesWithType
+	matchedReferenceResources.References.VirtualServices = refResource.VirtualServices
+	matchedReferenceResources.References.AppMeshResources = refResource.AppMeshResources
+	matchedReferenceResources.References.AmbassadorMappings = refResource.AmbassadorMappings
+
+	return matchedReferenceResources
+}
+
+// setServiceTypeAndManagedAnnotation This sets the managed annotation on each service as well as figures out what
+// type of service its is by looking at the rollout and set's its service type accordingly.
 func setServiceTypeAndManagedAnnotation(ro []v1alpha1.Rollout, refResource validation.ReferencedResources) {
-	//Go through and update all the services to be managed by their respective rollouts and set the type
 	for _, rollout := range ro {
 		for i, _ := range refResource.ServiceWithType {
 
 			if refResource.ServiceWithType[i].Service.Annotations == nil {
 				refResource.ServiceWithType[i].Service.Annotations = make(map[string]string)
 			}
-			refResource.ServiceWithType[i].Service.Annotations[v1alpha1.ManagedByRolloutsKey] = rollout.Name
 
 			if rollout.Spec.Strategy.Canary != nil {
 				if rollout.Spec.Strategy.Canary.CanaryService == refResource.ServiceWithType[i].Service.Name {
 					refResource.ServiceWithType[i].Type = validation.CanaryService
+					refResource.ServiceWithType[i].Service.Annotations[v1alpha1.ManagedByRolloutsKey] = rollout.Name
 				}
 				if rollout.Spec.Strategy.Canary.StableService == refResource.ServiceWithType[i].Service.Name {
 					refResource.ServiceWithType[i].Type = validation.StableService
+					refResource.ServiceWithType[i].Service.Annotations[v1alpha1.ManagedByRolloutsKey] = rollout.Name
 				}
 				if rollout.Spec.Strategy.Canary.PingPong != nil {
 					if rollout.Spec.Strategy.Canary.PingPong.PingService == refResource.ServiceWithType[i].Service.Name {
 						refResource.ServiceWithType[i].Type = validation.PingService
+						refResource.ServiceWithType[i].Service.Annotations[v1alpha1.ManagedByRolloutsKey] = rollout.Name
 					}
 					if rollout.Spec.Strategy.Canary.PingPong.PongService == refResource.ServiceWithType[i].Service.Name {
 						refResource.ServiceWithType[i].Type = validation.PongService
+						refResource.ServiceWithType[i].Service.Annotations[v1alpha1.ManagedByRolloutsKey] = rollout.Name
 					}
 				}
 			}
-			if rollout.Spec.Strategy.BlueGreen != nil && rollout.Spec.Strategy.BlueGreen.ActiveService == refResource.ServiceWithType[i].Service.Name {
-				refResource.ServiceWithType[i].Type = validation.ActiveService
+
+			if rollout.Spec.Strategy.BlueGreen != nil {
+				if rollout.Spec.Strategy.BlueGreen.ActiveService == refResource.ServiceWithType[i].Service.Name {
+					refResource.ServiceWithType[i].Type = validation.ActiveService
+					refResource.ServiceWithType[i].Service.Annotations[v1alpha1.ManagedByRolloutsKey] = rollout.Name
+				}
+				if rollout.Spec.Strategy.BlueGreen.PreviewService == refResource.ServiceWithType[i].Service.Name {
+					refResource.ServiceWithType[i].Type = validation.PreviewService
+					refResource.ServiceWithType[i].Service.Annotations[v1alpha1.ManagedByRolloutsKey] = rollout.Name
+				}
 			}
-			if rollout.Spec.Strategy.BlueGreen != nil && rollout.Spec.Strategy.BlueGreen.PreviewService == refResource.ServiceWithType[i].Service.Name {
-				refResource.ServiceWithType[i].Type = validation.PreviewService
+
+		}
+	}
+}
+
+// setIngressManagedAnnotation This tries to find ingresses that have matching services in the rollout resource and if so
+// it will add the managed by annotations just for linting so that we can later match up resources to a rollout resources
+// for the case when we have multiple rollout resources in a single manifest.
+func setIngressManagedAnnotation(ro []v1alpha1.Rollout, refResource validation.ReferencedResources) {
+	for _, rollout := range ro {
+		for i, _ := range refResource.Ingresses {
+			var serviceName string
+			if rollout.Spec.Strategy.Canary.TrafficRouting.Nginx != nil {
+				serviceName = rollout.Spec.Strategy.Canary.StableService
+			} else if rollout.Spec.Strategy.Canary.TrafficRouting.ALB != nil {
+				serviceName = rollout.Spec.Strategy.Canary.StableService
+				if rollout.Spec.Strategy.Canary.TrafficRouting.ALB.RootService != "" {
+					serviceName = rollout.Spec.Strategy.Canary.TrafficRouting.ALB.RootService
+				}
+			} else if rollout.Spec.Strategy.Canary.TrafficRouting.SMI != nil {
+				serviceName = rollout.Spec.Strategy.Canary.TrafficRouting.SMI.RootService
+			}
+
+			if ingressutil.HasRuleWithService(&refResource.Ingresses[i], serviceName) {
+				annotations := refResource.Ingresses[i].GetAnnotations()
+				annotations[v1alpha1.ManagedByRolloutsKey] = rollout.Name
+				refResource.Ingresses[i].SetAnnotations(annotations)
 			}
 		}
 	}
