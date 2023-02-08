912ca6cf7449c582c3fb4c2b00a1b4e9411ec648
Daniel Del Rio <daniel.delrio@getyourguide.com>
2023-02-08 14:47:38 +0100

    Remove port name in experiment_test.go
    Signed-off-by: Daniel Del Rio <daniel.delrio@getyourguide.com>

--- experiments/experiment_test.go
+++ experiments/experiment_test.go
@@ -70,7 +70,6 @@ func setExperimentService(template *v1alpha1.TemplateSpec) {
 	template.Service = &v1alpha1.TemplateService{}
 	template.Template.Spec.Containers[0].Ports = []corev1.ContainerPort{
 		{
-			Name:          "testport",
 			ContainerPort: 80,
 			Protocol:      "TCP",
 		},
