af6439a59c41d14cb93d203599bc0d0ee52054d6
AhmedGrati <ahmedgrati1999@gmail.com>
2023-07-19 20:27:42 +0100

    feat: add prometheus timeout attribute
    Signed-off-by: AhmedGrati <ahmedgrati1999@gmail.com>

--- pkg/apis/rollouts/v1alpha1/analysis_types.go
+++ pkg/apis/rollouts/v1alpha1/analysis_types.go
@@ -213,6 +213,9 @@ type PrometheusMetric struct {
 	// Sigv4 Config is the aws SigV4 configuration to use for SigV4 signing if using Amazon Managed Prometheus
 	// +optional
 	Authentication PrometheusAuth `json:"authentication,omitempty" protobuf:"bytes,3,opt,name=authentication"`
+	// Timeout represents the duration within which a prometheus query should complete. It is expressed in seconds.
+	// +optional
+	Timeout *int `json:"timeout,omitempty" protobuf:"bytes,4,opt,name=timeout"`
 }
 
 // PrometheusMetric defines the prometheus query to perform canary analysis
