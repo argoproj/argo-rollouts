dbcb41fd23d5ef8202e66784578a8dfaded67b6d
Zach Aller <zachaller@users.noreply.github.com>
2023-12-06 15:34:28 -0600

    change order
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- rollout/trafficrouting_test.go
+++ rollout/trafficrouting_test.go
@@ -1213,10 +1213,10 @@ func TestDontWeightToZeroWhenDynamicallyRollingBackToStable(t *testing.T) {
 	f.rolloutLister = append(f.rolloutLister, r2)
 	f.objects = append(f.objects, r2)
 
+	f.expectUpdateReplicaSetAction(rs1)                 // Updates the revision annotation from 1 to 3
+	scaleUpIndex := f.expectUpdateReplicaSetAction(rs1) // Updates the revision annotation from 1 to 3
 	f.expectUpdateRolloutAction(r2)
 	f.expectPatchRolloutAction(r2)
-	f.expectUpdateReplicaSetAction(rs1) // Updates the revision annotation from 1 to 3
-	scaleUpIndex := f.expectUpdateReplicaSetAction(rs1)
 
 	f.fakeTrafficRouting = newUnmockedFakeTrafficRoutingReconciler()
 	f.fakeTrafficRouting.On("UpdateHash", mock.Anything, mock.Anything, mock.Anything).Return(func(canaryHash, stableHash string, additionalDestinations ...v1alpha1.WeightDestination) error {
