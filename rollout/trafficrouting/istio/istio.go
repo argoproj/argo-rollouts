81c27d5470042cd54b445bde0aecb3edc8dc32dc
Zach Aller <zachaller@users.noreply.github.com>
2024-02-14 09:03:39 -0600

    clean up comments
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- rollout/trafficrouting/istio/istio.go
+++ rollout/trafficrouting/istio/istio.go
@@ -125,8 +125,6 @@ func (patches virtualServicePatches) patchVirtualService(httpRoutes []any, tlsRo
 }
 
 func (r *Reconciler) generateVirtualServicePatches(rolloutVsvcRouteNames []string, httpRoutes []VirtualServiceHTTPRoute, rolloutVsvcTLSRoutes []v1alpha1.TLSRoute, tlsRoutes []VirtualServiceTLSRoute, rolloutVsvcTCPRoutes []v1alpha1.TCPRoute, tcpRoutes []VirtualServiceTCPRoute, desiredWeight int64, additionalDestinations ...v1alpha1.WeightDestination) virtualServicePatches {
-	//canarySvc := r.rollout.Spec.Strategy.Canary.CanaryService
-	//stableSvc := r.rollout.Spec.Strategy.Canary.StableService
 	stableSvc, canarySvc := trafficrouting.GetStableAndCanaryServices(r.rollout)
 	canarySubset := ""
 	stableSubset := ""
@@ -720,7 +718,6 @@ func (r *Reconciler) reconcileVirtualServiceHeaderRoutes(virtualService v1alpha1
 		return err
 	}
 
-	//canarySvc := r.rollout.Spec.Strategy.Canary.CanaryService
 	_, canarySvc := trafficrouting.GetStableAndCanaryServices(r.rollout)
 	if destRuleHost != "" {
 		canarySvc = destRuleHost
@@ -1026,8 +1023,6 @@ func searchTcpRoute(tcpRoute v1alpha1.TCPRoute, istioTcpRoutes []VirtualServiceT
 
 // ValidateHTTPRoutes ensures that all the routes in the rollout exist
 func ValidateHTTPRoutes(r *v1alpha1.Rollout, routeNames []string, httpRoutes []VirtualServiceHTTPRoute) error {
-	//stableSvc := r.Spec.Strategy.Canary.StableService
-	//canarySvc := r.Spec.Strategy.Canary.CanaryService
 	stableSvc, canarySvc := trafficrouting.GetStableAndCanaryServices(r)
 
 	routeIndexesToPatch, err := getHttpRouteIndexesToPatch(routeNames, httpRoutes)
@@ -1065,8 +1060,6 @@ func ValidateHTTPRoutes(r *v1alpha1.Rollout, routeNames []string, httpRoutes []V
 
 // ValidateTlsRoutes ensures that all the routes in the rollout exist and they only have two destinations
 func ValidateTlsRoutes(r *v1alpha1.Rollout, vsvcTLSRoutes []v1alpha1.TLSRoute, tlsRoutes []VirtualServiceTLSRoute) error {
-	//stableSvc := r.Spec.Strategy.Canary.StableService
-	//canarySvc := r.Spec.Strategy.Canary.CanaryService
 	stableSvc, canarySvc := trafficrouting.GetStableAndCanaryServices(r)
 
 	routeIndexesToPatch, err := getTlsRouteIndexesToPatch(vsvcTLSRoutes, tlsRoutes)
@@ -1088,8 +1081,6 @@ func ValidateTlsRoutes(r *v1alpha1.Rollout, vsvcTLSRoutes []v1alpha1.TLSRoute, t
 
 // ValidateTcpRoutes ensures that all the routes in the rollout exist and they only have two destinations
 func ValidateTcpRoutes(r *v1alpha1.Rollout, vsvcTCPRoutes []v1alpha1.TCPRoute, tcpRoutes []VirtualServiceTCPRoute) error {
-	//stableSvc := r.Spec.Strategy.Canary.StableService
-	//canarySvc := r.Spec.Strategy.Canary.CanaryService
 	stableSvc, canarySvc := trafficrouting.GetStableAndCanaryServices(r)
 
 	routeIndexesToPatch, err := getTcpRouteIndexesToPatch(vsvcTCPRoutes, tcpRoutes)
@@ -1198,7 +1189,6 @@ func (r *Reconciler) reconcileVirtualServiceMirrorRoutes(virtualService v1alpha1
 	if err != nil {
 		return fmt.Errorf("[reconcileVirtualServiceMirrorRoutes] failed to get destination rule host: %w", err)
 	}
-	//canarySvc := r.rollout.Spec.Strategy.Canary.CanaryService
 	_, canarySvc := trafficrouting.GetStableAndCanaryServices(r.rollout)
 	if destRuleHost != "" {
 		canarySvc = destRuleHost
