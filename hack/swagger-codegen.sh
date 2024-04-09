5a310495506c14f240a200571bf2986787355574
Alexandre Gaudreault <alexandre_gaudreault@intuit.com>
2024-04-09 11:34:17 -0400

    fix deps version
    Signed-off-by: Alexandre Gaudreault <alexandre_gaudreault@intuit.com>

--- hack/swagger-codegen.sh
+++ hack/swagger-codegen.sh
@@ -6,4 +6,4 @@ PROJECT_ROOT=$(cd $(dirname ${BASH_SOURCE})/..; pwd)
 test -f "$PROJECT_ROOT/dist/swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar" || \
     curl https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/${SWAGGER_CODEGEN_VERSION}/swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar -o "$PROJECT_ROOT/dist/swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar"
 
-docker run --rm -v "$PROJECT_ROOT:/src" -w /src/ui -t maven:3-jdk-8 java -jar /src/dist/swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar "$@"
+docker run --rm -v "$PROJECT_ROOT:/src" -w /src/ui -t maven:3-jdk-11-slim java -jar /src/dist/swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar "$@"
