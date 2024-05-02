7ad6eb7f917571b962eab6cfad9f03c0d6309d37
Zach Aller <zachaller@users.noreply.github.com>
2024-05-02 14:43:01 -0500

    bump qps for e2e
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- test/fixtures/e2e_suite.go
+++ test/fixtures/e2e_suite.go
@@ -143,8 +143,8 @@ func (s *E2ESuite) SetupSuite() {
 	restConfig, err := config.ClientConfig()
 	s.CheckError(err)
 	s.Common.kubernetesHost = restConfig.Host
-	restConfig.Burst = defaults.DefaultBurst * 2
-	restConfig.QPS = defaults.DefaultQPS * 2
+	restConfig.Burst = defaults.DefaultBurst * 10
+	restConfig.QPS = defaults.DefaultQPS * 10
 	s.namespace, _, err = config.Namespace()
 	s.CheckError(err)
 	s.kubeClient, err = kubernetes.NewForConfig(restConfig)
