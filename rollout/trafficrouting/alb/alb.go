c108959cdaf7e56699448ed826b1d7f4eeea3c12
Zach Aller <zachaller@users.noreply.github.com>
2024-06-11 16:02:16 -0500

    refactor add unit test
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- rollout/trafficrouting/alb/alb.go
+++ rollout/trafficrouting/alb/alb.go
@@ -6,8 +6,6 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/argoproj/argo-rollouts/utils/weightutil"
-
 	rolloututil "github.com/argoproj/argo-rollouts/utils/rollout"
 
 	"github.com/sirupsen/logrus"
@@ -204,7 +202,7 @@ func (r *Reconciler) VerifyWeight(desiredWeight int32, additionalDestinations ..
 		return nil, nil
 	}
 
-	if !rolloututil.ShouldVerifyWeight(r.cfg.Rollout) && desiredWeight != weightutil.MaxTrafficWeight(r.cfg.Rollout) {
+	if !rolloututil.ShouldVerifyWeight(r.cfg.Rollout, desiredWeight) {
 		// If we should not verify weight but the ALB status has not been set yet due to a Rollout resource just being
 		// installed in the cluster we want to actually run the rest of the function, so we do not return if
 		// r.cfg.Rollout.Status.ALB is nil. However, if we should not verify, and we have already updated the status once
