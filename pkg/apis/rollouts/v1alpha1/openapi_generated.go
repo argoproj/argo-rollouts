3bd716a31c94df0908dac6581837355f91270817
Orlando Valdez <orlando.valdez@creditkarma.com>
2024-07-25 16:19:26 -0400

    Add missing codegen output after the merge
    Signed-off-by: Orlando Valdez <orlando.valdez@creditkarma.com>

--- pkg/apis/rollouts/v1alpha1/openapi_generated.go
+++ pkg/apis/rollouts/v1alpha1/openapi_generated.go
@@ -3364,6 +3364,21 @@ func schema_pkg_apis_rollouts_v1alpha1_NginxTrafficRouting(ref common.ReferenceC
 							},
 						},
 					},
+					"canaryIngressAnnotations": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"object"},
+							AdditionalProperties: &spec.SchemaOrBool{
+								Allows: true,
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: "",
+										Type:    []string{"string"},
+										Format:  "",
+									},
+								},
+							},
+						},
+					},
 				},
 			},
 		},
