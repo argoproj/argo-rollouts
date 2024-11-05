28cff29ed2ce8539b1b0d29ff822ca9be65bae85
Abhishek Bansal <abhibansal@abhisheks-mbp-7.corp.adobe.com>
2024-11-05 12:22:05 +0530

    feat: print error message in message field inside status in rollouts object
    Signed-off-by: Abhishek Bansal <abhibansal@abhisheks-mbp-7.corp.adobe.com>

--- rollout/controller.go
+++ rollout/controller.go
@@ -370,6 +370,32 @@ func (c *Controller) Run(ctx context.Context, threadiness int) error {
 	return nil
 }
 
+// function for printing reconciliation error message in status of rollout object.
+func (c *rolloutContext) UpdateStatus(ctx context.Context, newStatus v1alpha1.RolloutStatus, rollout *v1alpha1.Rollout) error {
+	prevStatus := c.rollout.Status
+	errMsgStatus := newStatus
+	logCtx := logutil.WithVersionFields(c.log, rollout)
+	patch, _, err := diff.CreateTwoWayMergePatch(
+		&v1alpha1.Rollout{
+			Status: prevStatus,
+		},
+		&v1alpha1.Rollout{
+			Status: *&errMsgStatus,
+		}, v1alpha1.Rollout{})
+	if err != nil {
+		logCtx.Errorf("Error constructing app status patch: %v", err)
+		return err
+	}
+	newRollout, err := c.argoprojclientset.ArgoprojV1alpha1().Rollouts(c.rollout.Namespace).Patch(ctx, c.rollout.Name, patchtypes.MergePatchType, patch, metav1.PatchOptions{}, "status")
+	if err != nil {
+		logCtx.Warningf("Error updating rollout: %v", err)
+		return err
+	}
+	c.newRollout = newRollout
+	return nil
+
+}
+
 // syncHandler compares the actual state with the desired, and attempts to
 // converge the two. It then updates the Phase block of the Rollout resource
 // with the current status of the resource.
@@ -448,6 +474,16 @@ func (c *Controller) syncHandler(ctx context.Context, key string) error {
 	err = roCtx.reconcile()
 	if err != nil {
 		logCtx.Errorf("roCtx.reconcile err %v", err)
+
+		newStatus := roCtx.calculateBaseStatus()
+		errMsg := fmt.Sprintf("Rollout Reconciliation failed due to %v", err)
+		newStatus.Message = errMsg
+
+		errStatus := roCtx.UpdateStatus(ctx, newStatus, rollout)
+
+		if errStatus != nil {
+			return errStatus
+		}
 		// return an err here so that we do not update the informer cache with a "bad" rollout object, for the case when
 		// we get an error during reconciliation but c.newRollout still gets updated this can happen in syncReplicaSetRevision
 		// https://github.com/argoproj/argo-rollouts/issues/2522#issuecomment-1492181154 I also believe there are other cases
