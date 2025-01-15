8df0d063597bcb646cdfc723b70348a97ef71bc7
Zach Aller <zachaller@users.noreply.github.com>
2025-01-15 11:45:34 -0600

    fix: fix lint
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- rollout/controller.go
+++ rollout/controller.go
@@ -4,16 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"os"
 	"reflect"
 	"strconv"
-	"strings"
 	"sync"
 	"time"
 
-	"github.com/argoproj/argo-rollouts/utils/annotations"
-
-	"github.com/argoproj/argo-rollouts/utils/diff"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts"
@@ -21,13 +16,11 @@ import (
 	log "github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	patchtypes "k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/dynamic"
@@ -1003,78 +996,78 @@ func (c *rolloutContext) updateReplicaSetFallbackToPatch(ctx context.Context, rs
 	if err != nil {
 		fmt.Printf("Error updating replicaset in updateReplicaSetFallbackToPatch %s: %s", rs.Name, err)
 		return nil, err
-		if errors.IsConflict(err) {
-			if os.Getenv("ARGO_ROLLOUTS_LOG_RS_DIFF_CONFLICT") == "true" {
-				rsGet, err := c.replicaSetLister.ReplicaSets(rs.Namespace).Get(rs.Name)
-				if err != nil {
-					return nil, fmt.Errorf("error getting replicaset in updateReplicaSetFallbackToPatch %s: %w", rs.Name, err)
-				}
-				rsGetJson, err := json.Marshal(rsGet)
-				if err != nil {
-					return nil, fmt.Errorf("error marshalling informer replicaset in updateReplicaSetFallbackToPatch %s: %w", rs.Name, err)
-				}
-				rsCopyJson, err := json.Marshal(rs)
-				if err != nil {
-					return nil, fmt.Errorf("error marshalling memory replicaset in updateReplicaSetFallbackToPatch %s: %w", rs.Name, err)
-				}
-				c.log.Infof("Informer RS: %s", rsGetJson)
-				c.log.Infof("Memory   RS: %s", rsCopyJson)
-			}
-
-			c.log.Infof("Conflict when updating replicaset %s, falling back to patch", rs.Name)
-
-			patchRS := appsv1.ReplicaSet{}
-			patchRS.Spec.Replicas = rs.Spec.Replicas
-			patchRS.Spec.Template.Labels = rs.Spec.Template.Labels
-			patchRS.Spec.Template.Annotations = rs.Spec.Template.Annotations
-
-			patchRS.Annotations = make(map[string]string)
-			patchRS.Labels = make(map[string]string)
-			patchRS.Spec.Selector = &metav1.LabelSelector{
-				MatchLabels: make(map[string]string),
-			}
-
-			if _, found := rs.Labels[v1alpha1.DefaultRolloutUniqueLabelKey]; found {
-				patchRS.Labels[v1alpha1.DefaultRolloutUniqueLabelKey] = rs.Labels[v1alpha1.DefaultRolloutUniqueLabelKey]
-			}
-
-			if _, found := rs.Annotations[v1alpha1.DefaultReplicaSetScaleDownDeadlineAnnotationKey]; found {
-				patchRS.Annotations[v1alpha1.DefaultReplicaSetScaleDownDeadlineAnnotationKey] = rs.Annotations[v1alpha1.DefaultReplicaSetScaleDownDeadlineAnnotationKey]
-			}
-
-			if _, found := rs.Spec.Selector.MatchLabels[v1alpha1.DefaultRolloutUniqueLabelKey]; found {
-				patchRS.Spec.Selector.MatchLabels[v1alpha1.DefaultRolloutUniqueLabelKey] = rs.Spec.Selector.MatchLabels[v1alpha1.DefaultRolloutUniqueLabelKey]
-			}
-
-			for key, value := range rs.Annotations {
-				if strings.HasPrefix(key, annotations.RolloutLabel) ||
-					strings.HasPrefix(key, "argo-rollouts.argoproj.io") ||
-					strings.HasPrefix(key, "experiment.argoproj.io") {
-					patchRS.Annotations[key] = value
-				}
-			}
-			for key, value := range rs.Labels {
-				if strings.HasPrefix(key, annotations.RolloutLabel) ||
-					strings.HasPrefix(key, "argo-rollouts.argoproj.io") ||
-					strings.HasPrefix(key, "experiment.argoproj.io") {
-					patchRS.Labels[key] = value
-				}
-			}
-
-			patch, _, err := diff.CreateTwoWayMergePatch(appsv1.ReplicaSet{}, patchRS, appsv1.ReplicaSet{})
-			if err != nil {
-				return nil, fmt.Errorf("error creating patch for conflict log in updateReplicaSetFallbackToPatch %s: %w", rs.Name, err)
-			}
-
-			c.log.Infof("Patching replicaset with patch: %s", string(patch))
-			updatedRS, err = c.kubeclientset.AppsV1().ReplicaSets(rs.Namespace).Patch(ctx, rs.Name, patchtypes.StrategicMergePatchType, patch, metav1.PatchOptions{})
-			if err != nil {
-				return nil, fmt.Errorf("error patching replicaset in updateReplicaSetFallbackToPatch %s: %w", rs.Name, err)
-			}
-
-		} else {
-			return nil, fmt.Errorf("error updating replicaset in updateReplicaSetFallbackToPatch %s: %w", rs.Name, err)
-		}
+		//if errors.IsConflict(err) {
+		//	if os.Getenv("ARGO_ROLLOUTS_LOG_RS_DIFF_CONFLICT") == "true" {
+		//		rsGet, err := c.replicaSetLister.ReplicaSets(rs.Namespace).Get(rs.Name)
+		//		if err != nil {
+		//			return nil, fmt.Errorf("error getting replicaset in updateReplicaSetFallbackToPatch %s: %w", rs.Name, err)
+		//		}
+		//		rsGetJson, err := json.Marshal(rsGet)
+		//		if err != nil {
+		//			return nil, fmt.Errorf("error marshalling informer replicaset in updateReplicaSetFallbackToPatch %s: %w", rs.Name, err)
+		//		}
+		//		rsCopyJson, err := json.Marshal(rs)
+		//		if err != nil {
+		//			return nil, fmt.Errorf("error marshalling memory replicaset in updateReplicaSetFallbackToPatch %s: %w", rs.Name, err)
+		//		}
+		//		c.log.Infof("Informer RS: %s", rsGetJson)
+		//		c.log.Infof("Memory   RS: %s", rsCopyJson)
+		//	}
+		//
+		//	c.log.Infof("Conflict when updating replicaset %s, falling back to patch", rs.Name)
+		//
+		//	patchRS := appsv1.ReplicaSet{}
+		//	patchRS.Spec.Replicas = rs.Spec.Replicas
+		//	patchRS.Spec.Template.Labels = rs.Spec.Template.Labels
+		//	patchRS.Spec.Template.Annotations = rs.Spec.Template.Annotations
+		//
+		//	patchRS.Annotations = make(map[string]string)
+		//	patchRS.Labels = make(map[string]string)
+		//	patchRS.Spec.Selector = &metav1.LabelSelector{
+		//		MatchLabels: make(map[string]string),
+		//	}
+		//
+		//	if _, found := rs.Labels[v1alpha1.DefaultRolloutUniqueLabelKey]; found {
+		//		patchRS.Labels[v1alpha1.DefaultRolloutUniqueLabelKey] = rs.Labels[v1alpha1.DefaultRolloutUniqueLabelKey]
+		//	}
+		//
+		//	if _, found := rs.Annotations[v1alpha1.DefaultReplicaSetScaleDownDeadlineAnnotationKey]; found {
+		//		patchRS.Annotations[v1alpha1.DefaultReplicaSetScaleDownDeadlineAnnotationKey] = rs.Annotations[v1alpha1.DefaultReplicaSetScaleDownDeadlineAnnotationKey]
+		//	}
+		//
+		//	if _, found := rs.Spec.Selector.MatchLabels[v1alpha1.DefaultRolloutUniqueLabelKey]; found {
+		//		patchRS.Spec.Selector.MatchLabels[v1alpha1.DefaultRolloutUniqueLabelKey] = rs.Spec.Selector.MatchLabels[v1alpha1.DefaultRolloutUniqueLabelKey]
+		//	}
+		//
+		//	for key, value := range rs.Annotations {
+		//		if strings.HasPrefix(key, annotations.RolloutLabel) ||
+		//			strings.HasPrefix(key, "argo-rollouts.argoproj.io") ||
+		//			strings.HasPrefix(key, "experiment.argoproj.io") {
+		//			patchRS.Annotations[key] = value
+		//		}
+		//	}
+		//	for key, value := range rs.Labels {
+		//		if strings.HasPrefix(key, annotations.RolloutLabel) ||
+		//			strings.HasPrefix(key, "argo-rollouts.argoproj.io") ||
+		//			strings.HasPrefix(key, "experiment.argoproj.io") {
+		//			patchRS.Labels[key] = value
+		//		}
+		//	}
+		//
+		//	patch, _, err := diff.CreateTwoWayMergePatch(appsv1.ReplicaSet{}, patchRS, appsv1.ReplicaSet{})
+		//	if err != nil {
+		//		return nil, fmt.Errorf("error creating patch for conflict log in updateReplicaSetFallbackToPatch %s: %w", rs.Name, err)
+		//	}
+		//
+		//	c.log.Infof("Patching replicaset with patch: %s", string(patch))
+		//	updatedRS, err = c.kubeclientset.AppsV1().ReplicaSets(rs.Namespace).Patch(ctx, rs.Name, patchtypes.StrategicMergePatchType, patch, metav1.PatchOptions{})
+		//	if err != nil {
+		//		return nil, fmt.Errorf("error patching replicaset in updateReplicaSetFallbackToPatch %s: %w", rs.Name, err)
+		//	}
+		//
+		//} else {
+		//	return nil, fmt.Errorf("error updating replicaset in updateReplicaSetFallbackToPatch %s: %w", rs.Name, err)
+		//}
 	}
 
 	//err = c.replicaSetInformer.GetIndexer().Update(updatedRS)
