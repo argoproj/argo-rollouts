844224a49f99d294580b7d0b4cac5b37dda5f2f4
Mujib Ahasan <ahasanmujib8@gmail.com>
2026-03-15 03:27:40 +0530

    feat: add optional namespace support for secretKeyRef in Analysis argument
    Signed-off-by: Mujib Ahasan <ahasanmujib8@gmail.com>

--- pkg/apis/rollouts/v1alpha1/openapi_generated.go
+++ pkg/apis/rollouts/v1alpha1/openapi_generated.go
@@ -5178,6 +5178,13 @@ func schema_pkg_apis_rollouts_v1alpha1_SecretKeyRef(ref common.ReferenceCallback
 							Format:      "",
 						},
 					},
+					"namespace": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Namespace is the namespace of the secret. If empty, the secret will be resolved from the namespace of the AnalysisRun/Rollout.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
 				},
 				Required: []string{"name", "key"},
 			},
