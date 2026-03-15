061b4f6c17d7f9cb021ac32e517cc5edf76b6170
Mujib Ahasan <ahasanmujib8@gmail.com>
2026-03-15 21:08:25 +0530

    test coverage added
    Signed-off-by: Mujib Ahasan <ahasanmujib8@gmail.com>

--- analysis/analysis_test.go
+++ analysis/analysis_test.go
@@ -2603,3 +2603,157 @@ func TestMaybeGarbageCollectAnalysisRunNoGCIfNoCompletedAt(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Empty(t, f.client.Fake.Actions())
 }
+
+func TestResolveArgsCrossNamespaceSecretRefDisabled(t *testing.T) {
+	f := newFixture(t)
+	defer f.Close()
+
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "token-secret",
+			Namespace: "shared-secrets",
+		},
+		Data: map[string][]byte{
+			"apiToken": []byte("shared-token"),
+		},
+	}
+
+	c, _, _ := f.newController(noResyncPeriodFunc)
+	f.kubeclient.CoreV1().Secrets("shared-secrets").Create(context.TODO(), secret, metav1.CreateOptions{})
+
+	c.enableCrossNamespaceSecretRefs = false
+	c.allowedSecretRefNamespaces = map[string]bool{}
+
+	args := []v1alpha1.Argument{
+		{
+			Name: "api-token",
+			ValueFrom: &v1alpha1.ValueFrom{
+				SecretKeyRef: &v1alpha1.SecretKeyRef{
+					Name:      "token-secret",
+					Key:       "apiToken",
+					Namespace: "shared-secrets",
+				},
+			},
+		},
+	}
+
+	tasks := []metricTask{
+		{
+			metric: v1alpha1.Metric{
+				Name: "webmetric",
+			},
+		},
+	}
+
+	_, _, err := c.resolveArgs(tasks, args, metav1.NamespaceDefault)
+	assert.Error(t, err)
+	assert.Equal(t, "cross-namespace secret references are disabled", err.Error())
+}
+
+func TestResolveArgsCrossNamespaceSecretRefNamespaceNotAllowed(t *testing.T) {
+	f := newFixture(t)
+	defer f.Close()
+
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "token-secret",
+			Namespace: "shared-secrets",
+		},
+		Data: map[string][]byte{
+			"apiToken": []byte("shared-token"),
+		},
+	}
+	c, _, _ := f.newController(noResyncPeriodFunc)
+	f.kubeclient.CoreV1().Secrets("shared-secrets").Create(context.TODO(), secret, metav1.CreateOptions{})
+
+	c.enableCrossNamespaceSecretRefs = true
+	c.allowedSecretRefNamespaces = map[string]bool{
+		"other-namespace": true,
+	}
+
+	args := []v1alpha1.Argument{
+		{
+			Name: "api-token",
+			ValueFrom: &v1alpha1.ValueFrom{
+				SecretKeyRef: &v1alpha1.SecretKeyRef{
+					Name:      "token-secret",
+					Key:       "apiToken",
+					Namespace: "shared-secrets",
+				},
+			},
+		},
+	}
+
+	tasks := []metricTask{
+		{
+			metric: v1alpha1.Metric{
+				Name: "webmetric",
+			},
+		},
+	}
+
+	_, _, err := c.resolveArgs(tasks, args, metav1.NamespaceDefault)
+	assert.Error(t, err)
+	assert.Equal(t, `cross-namespace secret references to namespace "shared-secrets" are not allowed`, err.Error())
+}
+
+func TestResolveArgsCrossNamespaceSecretRefAllowed(t *testing.T) {
+	f := newFixture(t)
+	defer f.Close()
+
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "token-secret",
+			Namespace: "shared-secrets",
+		},
+		Data: map[string][]byte{
+			"apiToken": []byte("shared-token"),
+		},
+	}
+	c, _, _ := f.newController(noResyncPeriodFunc)
+	f.kubeclient.CoreV1().Secrets("shared-secrets").Create(context.TODO(), secret, metav1.CreateOptions{})
+
+	c.enableCrossNamespaceSecretRefs = true
+	c.allowedSecretRefNamespaces = map[string]bool{
+		"shared-secrets": true,
+	}
+
+	args := []v1alpha1.Argument{
+		{
+			Name: "api-token",
+			ValueFrom: &v1alpha1.ValueFrom{
+				SecretKeyRef: &v1alpha1.SecretKeyRef{
+					Name:      "token-secret",
+					Key:       "apiToken",
+					Namespace: "shared-secrets",
+				},
+			},
+		},
+	}
+
+	tasks := []metricTask{
+		{
+			metric: v1alpha1.Metric{
+				Name: "webmetric",
+				Provider: v1alpha1.MetricProvider{
+					Web: &v1alpha1.WebMetric{
+						URL: "https://example.com",
+						Headers: []v1alpha1.WebMetricHeader{
+							{
+								Key:   "Authorization",
+								Value: "Bearer {{args.api-token}}",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	resolvedTasks, secrets, err := c.resolveArgs(tasks, args, metav1.NamespaceDefault)
+	assert.NoError(t, err)
+	assert.Len(t, resolvedTasks, 1)
+	assert.Len(t, secrets, 1)
+	assert.Equal(t, "shared-token", secrets[0])
+	assert.Equal(t, "Bearer shared-token", resolvedTasks[0].metric.Provider.Web.Headers[0].Value)
+}
