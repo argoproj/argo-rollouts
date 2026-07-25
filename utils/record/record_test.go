06c10086b7801974b2c0b3718220c652f692bc2e
rishh09 <ra0182108@gmail.com>
2026-07-26 00:49:54 +0530

    fix: honor self-service notification flag in event recorder
    Signed-off-by: rishh09 <ra0182108@gmail.com>

--- utils/record/record_test.go
+++ utils/record/record_test.go
@@ -27,6 +27,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes/fake"
+	k8srecord "k8s.io/client-go/tools/record"
 
 	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
 	argofake "github.com/argoproj/argo-rollouts/pkg/client/clientset/versioned/fake"
@@ -39,6 +40,60 @@ import (
 var (
 	noResyncPeriodFunc = func() time.Duration { return 0 }
 )
+
+type recordingAPIFactory struct {
+	api                       api.API
+	getAPICalls               int
+	getAPIsFromNamespaceCalls int
+	namespace                 string
+}
+
+func (f *recordingAPIFactory) GetAPI() (api.API, error) {
+	f.getAPICalls++
+	return f.api, nil
+}
+
+func (f *recordingAPIFactory) GetAPIsFromNamespace(namespace string) (map[string]api.API, error) {
+	f.getAPIsFromNamespaceCalls++
+	f.namespace = namespace
+	return map[string]api.API{namespace: f.api}, nil
+}
+
+func newTestEventRecorder(apiFactory api.Factory, selfServiceNotificationEnabled bool) *EventRecorderAdapter {
+	recorder := NewEventRecorder(
+		fake.NewSimpleClientset(),
+		prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "rollout_events_total",
+			},
+			[]string{"name", "namespace", "type", "reason"},
+		),
+		prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "notification_send_error",
+			},
+			[]string{"name", "namespace", "type", "reason"},
+		),
+		prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "notification_send_success",
+			},
+			[]string{"name", "namespace", "type", "reason"},
+		),
+		prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Name:    "notification_send_performance",
+				Help:    "Notification send performance.",
+				Buckets: []float64{0.01, 0.15, .25, .5, 1},
+			},
+			[]string{"namespace", "name"},
+		),
+		apiFactory,
+		selfServiceNotificationEnabled,
+	).(*EventRecorderAdapter)
+	recorder.Recorder = k8srecord.NewFakeRecorder(1000)
+	return recorder
+}
 
 func TestRecordLog(t *testing.T) {
 	prevOutput := log.StandardLogger().Out
@@ -99,6 +154,53 @@ func TestIncCounter(t *testing.T) {
 	m.Write(&buf)
 	assert.Equal(t, float64(3), *buf.Counter.Value)
 	assert.Equal(t, []string{"FooReason", "FooReason", "FooReason"}, rec.Events())
+}
+
+func TestEventRecorderNotificationAPIScope(t *testing.T) {
+	rollout := v1alpha1.Rollout{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "guestbook",
+			Namespace: "team-a",
+		},
+	}
+
+	tests := []struct {
+		name                           string
+		selfServiceNotificationEnabled bool
+		wantGetAPICalls                int
+		wantGetAPIsFromNamespaceCalls  int
+		wantNamespace                  string
+	}{
+		{
+			name:                           "self service disabled uses controller namespace API",
+			selfServiceNotificationEnabled: false,
+			wantGetAPICalls:                1,
+			wantGetAPIsFromNamespaceCalls:  0,
+		},
+		{
+			name:                           "self service enabled uses rollout namespace APIs",
+			selfServiceNotificationEnabled: true,
+			wantGetAPICalls:                0,
+			wantGetAPIsFromNamespaceCalls:  1,
+			wantNamespace:                  "team-a",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			mockAPI := mocks.NewMockAPI(mockCtrl)
+			mockAPI.EXPECT().GetConfig().Return(api.Config{}).AnyTimes()
+			apiFactory := &recordingAPIFactory{api: mockAPI}
+			rec := newTestEventRecorder(apiFactory, tt.selfServiceNotificationEnabled)
+
+			rec.Eventf(&rollout, EventOptions{EventReason: "FooReason"}, "something happened")
+
+			assert.Equal(t, tt.wantGetAPICalls, apiFactory.getAPICalls)
+			assert.Equal(t, tt.wantGetAPIsFromNamespaceCalls, apiFactory.getAPIsFromNamespaceCalls)
+			assert.Equal(t, tt.wantNamespace, apiFactory.namespace)
+		})
+	}
 }
 
 func TestSendNotifications(t *testing.T) {
